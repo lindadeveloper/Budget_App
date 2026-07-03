1e367b39bc307ed05cb6b09009544ec3cf5cd28f
Linda <lindacodesdev@gmail.com>
2026-07-03 15:47:10 -0700

    Updating CSS and deleteConfirmation
    Updating CSS and deleteConfirmation

---
+++ src/logic/deleteConfirmation.js
@@ -0,0 +1,26 @@
+export function deleteConfirmation(removeCategory) {
+  const deleteCategoryBtn = document.getElementById("deleteCatIconBtn");
+  const cancelCategoryBtn = document.getElementById("cancelCatIconBtn");
+  const confirmationDialog = document.getElementById("confirmationDialog");
+
+  const removeBtn = document.getElementById(`removeBtn-${currentId}`);
+  removeBtn.addEventListener("click", openConfirmation);
+
+  function openConfirmation() {
+    confirmationDialog.showModal();
+    deleteCategoryBtn.addEventListener("click", removeCategory, { once: true });
+    cancelCategoryBtn.addEventListener("click", cancelCategory, { once: true });
+  }
+
+  function cancelCategory() {
+    confirmationDialog.close();
+  }
+
+  function removeCategory() {
+    const categoryIcon = document.getElementById(`category-icon-${currentId}`);
+    categoryIcon.remove();
+    removeBtn.closest(".modal-icon").remove();
+    currentId = null;
+    confirmationDialog.close();
+  }
+}
